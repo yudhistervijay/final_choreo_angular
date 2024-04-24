2b1c2928a3d0b67bea68804afab1ab5269c7cae7
vijay <vijay.p@massiltech.com>
2024-04-24 11:53:17 +0530

    Update auth.config.ts

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -4,12 +4,12 @@ export const authCodeFlowConfig: AuthConfig = {
     issuer: 'https://api.asgardeo.io/t/orgenox1/oauth2/token',
     clientId: 'cMiX7ul5LdNI8CNVEeNqFMcFpvEa',
     responseType: 'code',
-    redirectUri: 'https://dc22e466-8476-4fbb-b9a1-aac39c2de156.e1-us-cdp-2.choreoapps.dev/dashboard',
+    redirectUri: 'https://83cf81d6-14e2-46e9-92bd-d4b559e3a595.e1-us-cdp-2.choreoapps.dev/dashboard',
    //redirectUri:'http://localhost:4200/dashboard',
     tokenEndpoint: 'https://api.asgardeo.io/t/orgenox1/oauth2/token',
     skipIssuerCheck: true,
     logoutUrl: 'https://api.asgardeo.io/t/orgenox1/oidc/logout',
-    postLogoutRedirectUri: 'https://dc22e466-8476-4fbb-b9a1-aac39c2de156.e1-us-cdp-2.choreoapps.dev/dashboard',
+    postLogoutRedirectUri: 'https://83cf81d6-14e2-46e9-92bd-d4b559e3a595.e1-us-cdp-2.choreoapps.dev/dashboard',
     scope: 'openid profile internal_user_mgt_view', 
     useSilentRefresh: false,
     silentRefreshTimeout: 50000000,
@@ -21,3 +21,4 @@ export const authCodeFlowConfig: AuthConfig = {
     strictDiscoveryDocumentValidation:false,
   
    };
+   
